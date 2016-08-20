7d70aef2b2f6ac5e50d0152de54bc50f15050666
Iain Munro <iain17@me.com>
2016-08-20 15:21:30 +0200

    Punten team1 and punten team2 are null sometimes.

--- src/Match.php
+++ src/Match.php
@@ -96,11 +96,13 @@ class Match extends AbstractItem
 
     /**
      * # doelpunten thuis club
+     * @var int|null
      */
     public $PuntenTeam1;
 
     /**
      * # doelpunten uit club
+     * @var int|null
      */
     public $PuntenTeam2;
 
